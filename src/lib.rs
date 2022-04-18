4d57814b1a710016b2c1ecf4e79dface8d8097b9
Jacob Cable <jacobcable94@gmail.com>
2022-04-18 20:09:01 +0100

    feat: convert images

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,40 @@
 use image::{open, DynamicImage};
+use ndarray::Array3;
 
-pub fn apply_convolution(path: &str, kernel: Vec<f32>) -> DynamicImage {
+pub fn convert_to_array(img: DynamicImage) -> Array3<f32> {
+    let width = usize::try_from(img.width()).unwrap();
+    let height = usize::try_from(img.height()).unwrap();
+
+    let mut array = Array3::<f32>::zeros((width, height, 3usize));
+
+    for pixel in img.to_rgb32f().enumerate_pixels() {
+        let x = usize::try_from(pixel.0).unwrap();
+        let y = usize::try_from(pixel.1).unwrap();
+        let rgb = pixel.2;
+        array[[x, y, 0]] = rgb[0];
+        array[[x, y, 1]] = rgb[1];
+        array[[x, y, 2]] = rgb[2];
+    }
+    array
+}
+
+pub fn apply_convolution(path: &str, kernel: [f32; 9]) -> DynamicImage {
     open(path).unwrap().filter3x3(&kernel)
 }
 
-pub fn convolution(a: ndarray::Array2<u32>, b: ndarray::Array2<u32>) -> ndarray::Array2<u32> {
+pub fn convolution(a: ndarray::Array2<f32>, b: &ndarray::Array2<f32>) -> ndarray::Array2<f32> {
     if a.shape()[0] < b.shape()[0] || a.shape()[1] < b.shape()[1] {
         panic!("input array is not big enough")
     }
 
-    let mut c = ndarray::Array::zeros((a.shape()[0] - 2, a.shape()[1] - 2));
+    let mut c = ndarray::Array2::<f32>::zeros((
+        1 + a.shape()[0] - b.shape()[0],
+        1 + a.shape()[1] - b.shape()[1],
+    ));
 
-    for r in 0..(a.shape()[0] - 2) {
-        for s in 0..(a.shape()[1] - 2) {
-            c[[r, s]] = 0;
+    // print!("{:?} \n \n {:?} \n \n", a, b);
+    for r in 0..c.shape()[0] {
+        for s in 0..c.shape()[1] {
             for i in 0..b.shape()[0] {
                 for j in 0..b.shape()[1] {
                     c[[r, s]] += a[[r + i, s + j]] * b[[i, j]];
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 mod lib;
+use image::open;
+use ndarray::{arr2, Axis};
 
 fn main() {
-    let image_array = lib::get_image_array("pxArt.png");
+    // let kernel = [-1.0f32, -1.0, -1.0, -1.0, 8.0, -1.0, -1.0, -1.0, -1.0];
 
-    println!("{}", image_array.slice(ndarray::s![.., .., 2]));
+    // lib::apply_convolution("pxArt.png", kernel);
+
+    let img = open("hotdog-pixel.png").unwrap();
+
+    let array = lib::convert_to_array(img);
+    let _r = array.index_axis(Axis(2), 0).to_owned();
+    let _g = array.index_axis(Axis(2), 1).to_owned();
+    let _b = array.index_axis(Axis(2), 2).to_owned();
+
+    let k = arr2(&[[-1f32, -1.0, -1.0], [-1f32, 8.0, -1.0], [-1f32, -1.0, -1.0]]);
+
+    let conv_r = lib::convolution(_r, &k);
+    let conv_g = lib::convolution(_g, &k);
+    let conv_b = lib::convolution(_b, &k);
+
+    print!("{:?}", conv_r);
+    print!("{:?}", conv_g);
+    print!("{:?}", conv_b);
 }
