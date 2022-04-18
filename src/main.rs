adedfa9c1fb90377d8b3336d37ea19f5979fd343
Jacob Cable <jacobcable94@gmail.com>
2022-04-18 21:08:49 +0100

    feat: save new images

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod lib;
-use image::open;
-use ndarray::{arr2, Axis};
+use image::{open, GrayImage, Luma};
+use ndarray::{arr2, Array2, Axis};
 
 fn main() {
     // let kernel = [-1.0f32, -1.0, -1.0, -1.0, 8.0, -1.0, -1.0, -1.0, -1.0];
@@ -20,7 +20,32 @@ fn main() {
     let conv_g = lib::convolution(_g, &k);
     let conv_b = lib::convolution(_b, &k);
 
-    print!("{:?}", conv_r);
-    print!("{:?}", conv_g);
-    print!("{:?}", conv_b);
+    // print!("{:?}", conv_r);
+    // print!("{:?}", conv_g);
+    // print!("{:?}", conv_b);
+
+    let sum = conv_r + conv_g + conv_b;
+
+    let min = sum.iter().fold(f32::INFINITY, |a, &b| a.min(b));
+
+    let normalized = sum.map(|x| x - min);
+    let max = normalized.iter().fold(-1.0f32, |a, &b| a.max(b));
+    let normalized_2 = normalized.map(|x| (255f32 * (x / max)) as u8);
+    print!("{:?}", sum);
+    print!("{:?}", normalized_2);
+    // let buffer = GrayImage::from_raw(
+    //     sum.shape()[0] as u32,
+    //     sum.shape()[1] as u32,
+    //     sum.into_raw_vec(),
+    // );
+
+    let mut img = GrayImage::new(30, 21);
+
+    for x in 0..29u32 {
+        for y in 0..20u32 {
+            let val = normalized_2[[x as usize, y as usize]];
+            img.put_pixel(x, y, Luma([val]));
+        }
+    }
+    img.save("test.png").unwrap();
 }
